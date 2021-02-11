0228b22c40b9eaad7b9b4f4ac88547e21ada3dc1
Stefan Garces <stefangarces@gmail.com>
2021-02-11 23:24:46 +0100

    Back to XMLHttpRequest.

--- Script.js
+++ Script.js
@@ -1,47 +1,12 @@
-const SearchBtn = document.getElementById("searchButton");
+//const searchBtn = document.createElementById("searchButton");
+const weatherOutput = document.getElementById("weatherText");
 
-SearchBtn.onclick = async function() {
-    let apiKey = "34832f1e903a4e490cfc9a2d3fffea23";
-    let cityName = "Göteborg";
-    let response = await fetch ('https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${apiKey}')
-    let json = await response.json()
-    console.log(json)
-    setWeather(json)
+const url = new URL('https://api.openweathermap.org/data/2.5/weather');
 
-    console.log(response)
-}
+url.searchParams.append("q", "Eskilstuna");
+url.searchParams.append("appid", "34832f1e903a4e490cfc9a2d3fffea23");
+url.searchParams.append("mode", "json");
+url.searchParams.append("units", "metric");
+url.searchParams.append("lang", "se");
 
-
-
-
-
-/*
-const searchBtn = document.getElementsByClassName("searchButton");
-
-function search(){
-
-    let searchBox = document.getElementsByClassName('searchBox');
-    let userSearch = searchBox.value;
-    
-    console.log(userSearch);
-    
-    let onlyWeather = document.getElementById('onlyWeather');
-    let onlyAttraction = document.getElementById('onlyAttraction');
-
-    let weather = document.querySelector('#weather');
-    let attraction = document.querySelector('#attraction');
-
-    if (onlyWeather.checked == true) {
-        weather.style.display = 'flex';
-    } else {
-        weather.style.display = 'none';
-    }
-
-    if (onlyAttraction.checked == true) {
-        attraction.style.display = 'grid';
-    } else {
-        attraction.style.display = 'none';
-    }
-
-}
-*/
+weatherOutput.innerText = url;
