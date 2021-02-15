897a6ee1597b0516208adf42b98154462003ec75
Stefan Garces <stefangarces@gmail.com>
2021-02-15 18:53:01 +0100

    Trying to fetch the api

--- Script.js
+++ Script.js
@@ -1,12 +1,44 @@
-//const searchBtn = document.createElementById("searchButton");
-const weatherOutput = document.getElementById("weatherText");
+const searchBox = document.getElementById("searchBox");
+const searchBtn = document.getElementById("searchButton");
+let input = searchBox.value;
 
-const url = new URL('https://api.openweathermap.org/data/2.5/weather');
+searchBtn.addEventListener('click', () => {
+    let checkWeather = document.getElementById('onlyWeather');
+    let checkAttraction = document.getElementById('onlyAttractions');
+    getWeather();
+})
 
-url.searchParams.append("q", "Eskilstuna");
-url.searchParams.append("appid", "34832f1e903a4e490cfc9a2d3fffea23");
-url.searchParams.append("mode", "json");
-url.searchParams.append("units", "metric");
-url.searchParams.append("lang", "se");
+async function getWeather() {
+    // let cityName = ;
+    let response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=gothenburg&units=metric&appid=34832f1e903a4e490cfc9a2d3fffea23`);
+    let json = await response.json();
+    console.log(response.status); // 200
+    console.log(response.statusText); // OK
+    //Weather(json);
+    //City(json);
+    console.log(json);
+}
 
-weatherOutput.innerText = url;
+function City(json) {
+    let city = document.getElementById("city");
+    city.innerHTML = json.name;
+}
+
+function Weather(json) {
+    let temp = document.getElementById('temp');
+    let weather = document.getElementById('cityWeather');
+    let weathercon = json.weather[0].icon;
+    document.getElementById('weathercon').src = `http://openweathermap.org/img/w/${weathercon}.png`;
+
+    temp.innerHTML = json.main.temp + " C°";
+    weather.innerHTML = json.weather[0].description;
+
+    let city = json.name;
+    let weatherTitle = document.getElementById("weatherText");
+    weatherTitle.innerHTML = `Weather in ${city}`;
+} 
+
+function getAttraction() {
+    let cityInfo = document.getElementById("cityInfo");
+    cityInfo.innerHTML = json.name;
+}
