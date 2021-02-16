8fedd2861b6f1d3ce2869c51a5a6c58fbd641acc
Stefan Garces <stefangarces@gmail.com>
2021-02-16 17:30:22 +0100

    Working on the API for the attractions.

--- Script.js
+++ Script.js
@@ -2,14 +2,13 @@ const searchBox = document.getElementById("searchBox");
 const searchBtn = document.getElementById("searchButton");
 
 searchBtn.addEventListener('click', () => {
-    let checkWeather = document.getElementById('onlyWeather');
-    let checkAttraction = document.getElementById('onlyAttractions');
-    getWeather();
+    //getWeather();
+    getAttraction();
 })
 
 async function getWeather() {
     let cityName = searchBox.value;
-    let response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=34832f1e903a4e490cfc9a2d3fffea23`);
+    let response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&lang=en&appid=34832f1e903a4e490cfc9a2d3fffea23`);
     let json = await response.json();
     Weather(json);
     City(json);
@@ -22,18 +21,33 @@ function City(json) {
 }
 
 function Weather(json) {
-    let temp = document.getElementById('temp');
+    let temp = document.getElementById("temp");
     let weather = document.getElementById("cityWeather");
     let weatherDes = json.weather[0].description;
     let tempDes = json.main.temp;
     weather.innerHTML = weatherDes;
     temp.innerHTML = tempDes + " °C";
-    console.log(json);
 
 } 
 
-/*
-function getAttraction() {
+
+async function getAttraction() {
+    let cityName = searchBox.value;
+    const cID = "F0XVM54UYVNCOZHAZDT4RSKENV3X5QN2DH2WALP0UDVQGYYI";
+    const cSecret = "VMJ3P5SXFWKQVGF0ZRW4AUFWB25YZ4YFMRQUYT0KC0F2WCCI";
+
+    let cDate = new Date();
+    let Day = "0" + (cDate.getDate());
+    let Month = "0" + (cDate.getMonth() + 1);
+    let Year = cDate.getFullYear();
+    let date = `${Year}${Month}${Day}`;
+    
+    let response = await fetch(`https://api.foursquare.com/v2/venues/search?near=${cityName}&client_id=${cID}&client_secret=${cSecret}&v=${date}`);
+        let json = await response.json();
+        console.log(json);
+    
+    /*
     let cityInfo = document.getElementById("cityInfo");
-    cityInfo.innerHTML = json.name;
-} */
+    cityInfo.innerHTML = json.name; */
+}
+
